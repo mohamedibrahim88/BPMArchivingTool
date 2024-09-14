1df66292b42ed314321547a15fc7df0dd3440b9b
yjo99 <yousef16992@gmail.com>
2024-09-14 17:04:13 +0300

    Ignore SSL certificate validation for communication

--- src/main/java/com/archiving/archivingTool/Configuration/AppConfig.java
+++ src/main/java/com/archiving/archivingTool/Configuration/AppConfig.java
@@ -1,58 +1,44 @@
 package com.archiving.archivingTool.Configuration;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContexts;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.stereotype.*;
+
+
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import javax.net.ssl.SSLContext;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+
 
 @Configuration
 public class AppConfig {
 
 //    @Bean
 //    public RestTemplate restTemplate (){
-////        SSLContext sslContext = TrustAllCertificates.createTrustAllContext();
-////        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
-////                sslContext,
-////                NoopHostnameVerifier.INSTANCE
-////        );
-////
-////        CloseableHttpClient httpClient = HttpClients.custom()
-////                .setSSLSocketFactory(socketFactory)
-////                .build();
-////
-////        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-////        return new RestTemplate(factory);
-//
-//        /////////////////////////////
-//        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-//        factory.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy-host", proxy-port)));
-//        return new RestTemplate(factory);
+//        return new RestTemplate();
 //    }
 
     @Bean
-    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         HttpComponentsClientHttpRequestFactory requestFactoryHttp = new HttpComponentsClientHttpRequestFactory();
 
-        TrustStrategy acceptingTrustStrategy = (TrustStrategy) (cert, authType) -> true;        SSLContext sslContext = SSLContexts.custom()
+        TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+        SSLContext sslContext = SSLContexts.custom()
                 .loadTrustMaterial(null, acceptingTrustStrategy)
                 .build();
         SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
@@ -66,8 +52,7 @@ public class AppConfig {
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(connectionManager)
                 .build();
-        requestFactoryHttp.setHttpClient((HttpClient) httpClient);
+        requestFactoryHttp.setHttpClient(httpClient);
         return new RestTemplate(requestFactoryHttp);
     }
-
 }
