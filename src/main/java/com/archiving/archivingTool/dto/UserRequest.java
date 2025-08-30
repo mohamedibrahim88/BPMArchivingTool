87521aee254ac79a9198b05cc1869260314d3304
yjo99 <yousef16992@gmail.com>
2025-08-30 07:47:11 -0700

    create validation on data create user

--- src/main/java/com/archiving/archivingTool/dto/UserRequest.java
+++ src/main/java/com/archiving/archivingTool/dto/UserRequest.java
@@ -1,10 +1,28 @@
 package com.archiving.archivingTool.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class UserRequest {
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
     private String password;
+
     private String firstName;
+
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
 
     // getters and setters
